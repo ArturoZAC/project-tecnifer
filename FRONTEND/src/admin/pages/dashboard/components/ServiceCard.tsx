d45b2b98b04a0b5fea5d49e3c5d2705ec4565b8d
ArturoZAC <arturoyz2105@gmail.com>
2026-01-09 18:37:34 -0500

    addproperties

--- FRONTEND/src/admin/pages/dashboard/components/ServiceCard.tsx
+++ FRONTEND/src/admin/pages/dashboard/components/ServiceCard.tsx
@@ -12,22 +12,29 @@ export const ServiceCard = ({ service }: ServiceCardProps) => {
   const navigate = useNavigate();
 
   return (
-    <Card className="overflow-hidden shadow-card hover:shadow-lg transition-all duration-300 animate-fade-in bg-card border-border group py-0">
-      <CardHeader className="p-0">
-        <div className="relative h-60 overflow-hidden">
-          <img
-            src={service.image}
-            alt={service.title}
-            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-          />
-          <div className="absolute inset-0 bg-linear-to-t from-foreground/20 to-transparent" />
-        </div>
+    <Card className="overflow-hidden shadow-card hover:shadow-lg transition-all duration-300 animate-fade-in bg-card border-border group py-0 gap-2">
+      {/* Imagen */}
+      <div className="relative h-60 overflow-hidden">
+        <img
+          src={service.image}
+          alt={service.title}
+          className="w-full h-full object-cover object-center transition-transform duration-500 group-hover:scale-110"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-foreground/20 to-transparent" />
+      </div>
+
+      {/* Header (solo si quieres título + algo extra) */}
+      <CardHeader className="pb-0 grid-cols-none grid-rows-none">
+        <h3 className="text-lg font-semibold text-card-foreground">{service.title}</h3>
       </CardHeader>
-      <CardContent className="p-5">
-        <h3 className="text-lg font-semibold text-card-foreground mb-2">{service.title}</h3>
-        <p className="text-sm text-muted-foreground line-clamp-2">{service.description}</p>
+
+      {/* Contenido */}
+      <CardContent className="px-3 py-3">
+        <p className="text-sm text-muted-foreground line-clamp-2 h-12">{service.description}</p>
       </CardContent>
-      <CardFooter className="p-5 pt-0 flex gap-2">
+
+      {/* Footer acciones */}
+      <CardFooter className="p-3 flex gap-2">
         <Button
           variant="outline"
           size="sm"
@@ -37,10 +44,10 @@ export const ServiceCard = ({ service }: ServiceCardProps) => {
           <Pencil className="w-4 h-4 mr-2" />
           Editar
         </Button>
+
         <Button
           variant="ghost"
           size="sm"
-          // onClick={() => removeService(service.id)}
           className="text-destructive hover:text-destructive bg-destructive/10 hover:bg-destructive/20 flex-1"
         >
           <Trash2 className="w-4 h-4" />
